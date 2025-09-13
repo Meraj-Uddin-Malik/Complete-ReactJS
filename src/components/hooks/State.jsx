1babc24de2b05d5d962d25fcab007008092f0e9a
Meraj-Uddin-Malik <141-2021@lms.indus.edu.pk>
2025-09-14 04:17:48 +0500

    Refactor State component structure and improve rendering logic; comment out height in CSS

--- src/components/hooks/State.jsx
+++ src/components/hooks/State.jsx
@@ -3,6 +3,7 @@ import {useState } from "react";
 export const State = () => {
 
 const [count, setCount] = useState(0);
+console.log("Parent Component Rendered");
   const handleButtonClick = () => {
     setCount(() => count + 1);
   };
@@ -10,9 +11,21 @@ const [count, setCount] = useState(0);
 
 
   return (
+    <>
     <section className="main-div">
       <h1>{count}</h1>
       <button onClick={handleButtonClick}>Increment Value</button>
     </section>
+    <ChildComponent/>
+    </>
   );
 };
+
+function ChildComponent() {
+    console.log("Child Component Rendered");
+  return (
+    <div className="main-div">
+      <h1>This is Child Component</h1>
+    </div>
+  );
+}   
