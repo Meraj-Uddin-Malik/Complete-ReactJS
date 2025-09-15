28c3ecf767d4528c8167bf004beac49615649de7
Meraj-Uddin-Malik <141-2021@lms.indus.edu.pk>
2025-09-16 01:34:48 +0500

    Refactor state management components: remove unused components, update imports, and implement Counter component with increment, decrement, and reset functionality.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-
-import DrivedState from "./components/hooks/useState/UseStatePractice";
+import React from "react";
+// import NetflixSeries from "./components/NetflixSeries";
+// import DrivedState from "./components/hooks/useState/UseStatePractice";
+import Counter from "./components/ReactHooks/useState/UseStatePractice_01";
 function App() {
   return (
     <section className="container">
       {/* <h1 className="card-heading">List Of Best Netflix Series</h1>
       <NetflixSeries /> */}
-      <DrivedState />
+      {/* <DrivedState /> */}
+      <Counter />
     </section>
   );
 }
