0be3a130a649cd52821cbd668663afb3e4e62ba2
Meraj-Uddin-Malik <141-2021@lms.indus.edu.pk>
2025-09-16 13:41:08 +0500

    Implement LiftingState component with input and display functionality; remove TodoList from App

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import React from "react";
-import TodoList from "./components/ReactHooks/useState/UseStatePractice_04";
+import { LiftingState } from "./components/LiftStateUp";
+
 function App() {
   return (
     <section className="container">
-      {/* <h1 className="card-heading">List Of Best Netflix Series</h1>
-      <NetflixSeries /> */}
-      {/* <DrivedState /> */}
-      <TodoList />
+      
+      <LiftingState />
     </section>
   );
 }
