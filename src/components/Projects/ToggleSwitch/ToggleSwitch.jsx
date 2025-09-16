4df226316efd55ad386031c52c43682bea9353dd
Meraj-Uddin-Malik <141-2021@lms.indus.edu.pk>
2025-09-16 20:07:53 +0500

    Fix ToggleSwitch component to correctly display switch state

--- src/components/Projects/ToggleSwitch/ToggleSwitch.jsx
+++ src/components/Projects/ToggleSwitch/ToggleSwitch.jsx
@@ -4,8 +4,12 @@ import "./Toggle.css";
 export default function ToggleSwitch() {
   const [isOn, setIsOn] = useState(true);
 
+  const handleToggleSwitch = () => {
+    setIsOn(!isOn);
+  }
+
   return (
-    <div className="toggle-switch">
+    <div className="toggle-switch" onClick={handleToggleSwitch}>
       <div className="switch">
         <span className="swtich-state">{isOn? "ON": "OFF"}</span>
       </div>
