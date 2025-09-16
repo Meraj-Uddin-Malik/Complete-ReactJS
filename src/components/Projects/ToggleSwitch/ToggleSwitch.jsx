5b9527215802484870bfeff91f6afcbe59e9777d
Meraj-Uddin-Malik <141-2021@lms.indus.edu.pk>
2025-09-17 00:04:46 +0500

    Refactor ToggleSwitch component to improve state display and styling

--- src/components/Projects/ToggleSwitch/ToggleSwitch.jsx
+++ src/components/Projects/ToggleSwitch/ToggleSwitch.jsx
@@ -6,12 +6,19 @@ export default function ToggleSwitch() {
 
   const handleToggleSwitch = () => {
     setIsOn(!isOn);
-  }
+  };
+
+  const checkIsOn = isOn ? "on" : "off";
+  const toggleBgColor = { backgroundColor: isOn ? "#4caf50" : "#f44336" };
 
   return (
-    <div className="toggle-switch" onClick={handleToggleSwitch}>
-      <div className="switch">
-        <span className="swtich-state">{isOn? "ON": "OFF"}</span>
+    <div
+      className="toggle-switch"
+      style={toggleBgColor}
+      onClick={handleToggleSwitch}
+    >
+      <div className={`switch ${checkIsOn}`}>
+        <span className="swtich-state">{checkIsOn}</span>
       </div>
     </div>
   );
