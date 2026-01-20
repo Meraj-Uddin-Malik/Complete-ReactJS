13f53134bfe66c98f3dcd449092d0b220391ddad
Meraj-Uddin-Malik <merajuddinmalik136@gmail.com>
2026-01-21 01:36:27 +0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
-
-import NetflixComponent from "./components/NetflixComponent";
-function App() {
+export const App = () => {
   return (
-    <section className="container">
-      <NetflixComponent />
-    </section>
+    <>
+      <NetflixSeries />
+    </>
   );
-}
+};
 
 export default App;
-        
+
+const NetflixSeries = () => {
+  return (
+    <div>
+      <div>
+        <img
+          src="./images/qot.jpg"
+          alt="Queens Of Tears"
+          height={300}
+          width={500}
+        />
+      </div>
+      <h2>Name: Queens Of Tears</h2>
+      <h3>Rating: 8.2</h3>
+      <p>Summary: A gripping tale of love and loss and redemption and hope.</p>
+    </div>
+  );
+};
