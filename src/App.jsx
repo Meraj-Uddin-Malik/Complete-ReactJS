2440383f3cacabf57581d120f35ac2a6b5297ac9
Meraj-Uddin-Malik <merajuddinmalik136@gmail.com>
2026-01-21 01:48:25 +0500

    feat: move NetflixSeries component to its own file for better organization

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,11 @@
+import NetflixSeries from "./components/NetflixComponent";
+
 export const App = () => {
   return (
     <>
-      <NetflixSeries />
+     <NetflixSeries />
     </>
   );
 };
 
 export default App;
-
-const NetflixSeries = () => {
-  return (
-    <div>
-      <div>
-        <img
-          src="./images/qot.jpg"
-          alt="Queens Of Tears"
-          height={300}
-          width={500}
-        />
-      </div>
-      <h2>Name: Queens Of Tears</h2>
-      <h3>Rating: 8.2</h3>
-      <p>Summary: A gripping tale of love and loss and redemption and hope.</p>
-    </div>
-  );
-};
---
+++ src/components/NetflixComponent.jsx
@@ -0,0 +1,18 @@
+const NetflixSeries = () => {
+  return (
+    <div>
+      <div>
+        <img
+          src="./images/qot.jpg"
+          alt="Queens Of Tears"
+          height={300}
+          width={500}
+        />
+      </div>
+      <h2>Name: Queens Of Tears</h2>
+      <h3>Rating: 8.2</h3>
+      <p>Summary: A gripping tale of love and loss and redemption and hope.</p>
+    </div>
+  );
+};
+export default NetflixSeries;
