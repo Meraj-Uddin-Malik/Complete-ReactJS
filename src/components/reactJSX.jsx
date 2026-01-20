83a75a41243b064d7d1cd4cfbc0aa3f4e39db31c
Meraj-Uddin-Malik <merajuddinmalik136@gmail.com>
2026-01-21 00:59:03 +0500

    chore: remove unused ReactApp component to streamline codebase

--- src/components/reactJSX.jsx
+++
@@ -1,9 +0,0 @@
-export const ReactApp = () => {
-  return (
-    <div>
-        <h1>Hello, React JSX!</h1>
-        <p>Welcome to your React application.</p>
-    </div>
-  )
-}
-export default ReactApp
