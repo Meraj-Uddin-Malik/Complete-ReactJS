191d5f2f5f878d0d8adaf9ea9ae7591cd7537bcc
Meraj-Uddin-Malik <merajuddinmalik136@gmail.com>
2026-01-22 23:54:23 +0500

    fix: simplify age condition and enhance rendering in NetflixSeries component

--- src/components/NetflixComponent.jsx
+++ src/components/NetflixComponent.jsx
@@ -3,44 +3,24 @@ const NetflixSeries = () => {
   const rating = 8.2;
   const genre = "Romcom";
   const summary = "A gripping tale of love and loss and redemption and hope.";
+  let age = 17;
 
-  const age = 12; // Example age, can be modified to test the condition
-  if (age > 18) {
-    return (
+  return (
+    <div>
       <div>
-        <div>
-          <img
-            src="./images/qot.jpg"
-            alt="Queens Of Tears"
-            height={300}
-            width={500}
-          />
-        </div>
-        <h2>{seriesName}</h2>
-        <h3>Rating: {rating}</h3>
-        <h4>Genre: {genre}</h4>
-        <p>Summary: {summary}</p>
-        <button>Allowed</button>
+        <img
+          src="./images/qot.jpg"
+          alt="Queens Of Tears"
+          height={300}
+          width={500}
+        />
       </div>
-    );
-  } else {
-    return (
-      <div>
-        <div>
-          <img
-            src="./images/qot.jpg"
-            alt="Queens Of Tears"
-            height={300}
-            width={500}
-          />
-        </div>
-        <h2>{seriesName}</h2>
-        <h3>Rating: {rating}</h3>
-        <h4>Genre: {genre}</h4>
-        <p>Summary: {summary}</p>
-        <button>Not Allowed</button>
-      </div>
-    );
-  }
+      <h2>{seriesName}</h2>
+      <h3>Rating: {rating}</h3>
+      <h4>Genre: {genre}</h4>
+      <p>Summary: {summary}</p>
+      <button>{age >= 12 ? "Allowed" : "Not Allowed"}</button>
+    </div>
+  );
 };
 export default NetflixSeries;
