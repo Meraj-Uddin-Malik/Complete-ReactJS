13dc80c683770557d99d0840c3b1cdfedddb6981
Meraj-Uddin-Malik <merajuddinmalik136@gmail.com>
2026-02-02 00:13:59 +0500

    chore: add missing newline at the beginning of SeriesCard component

--- src/components/SeriesCard.jsx
+++ src/components/SeriesCard.jsx
@@ -1,20 +1,22 @@
 export const SeriesCard = (props) => {
+  const {id, name, img_url, rating, genre, description, cast, watch_url } =
+    props.data;
   return (
-    <li key={props.currentSeries.id}>
+    <li key={id}>
       <div>
         <img
-          src={props.currentSeries.img_url}
+          src={img_url}
           alt="qot"
           height="40%"
           width="40%"
         />
       </div>
-      <h2>{props.currentSeries.name}</h2>
-      <h3>Rating: {props.currentSeries.rating}</h3>
-      <h4>Genre: {props.currentSeries.genre}</h4>
-      <p>Summary: {props.currentSeries.description}</p>
-      <p>Cast: {props.currentSeries.cast}</p>
-      <a href={props.currentSeries.watch_url} target="_blank">
+      <h2>{name}</h2>
+      <h3>Rating: {rating}</h3>
+      <h4>Genre: {genre}</h4>
+      <p>Summary: {description}</p>
+      <p>Cast: {cast}</p>
+      <a href={watch_url} target="_blank">
         <button>Watch Now</button>
       </a>
     </li>
