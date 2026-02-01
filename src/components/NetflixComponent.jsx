8ca71dbc793bed5e1ad225e7a504f435164e0f17
Meraj-Uddin-Malik <merajuddinmalik136@gmail.com>
2026-02-01 23:19:32 +0500

    feat: import seriesData for rendering in NetflixSeries component

--- src/components/NetflixComponent.jsx
+++ src/components/NetflixComponent.jsx
@@ -1,10 +1,11 @@
 import { SeriesCard } from "./SeriesCard";
+import seriesData from "../api/seriesData.json";
 
 const NetflixSeries = () => {
   return (
     <ul>
       {seriesData.map((currentSeries) => {
-       <SeriesCard />
+       <SeriesCard key={currentSeries.id} currentSeries={currentSeries} />
       })}
     </ul>
   );
--- src/components/SeriesCard.jsx
+++ src/components/SeriesCard.jsx
@@ -1,17 +1,22 @@
 import seriesData from "../api/seriesData.json";
 
-export const SeriesCard = () => {
+export const SeriesCard = (props) => {
   return (
-    <li key={currentSeries.id}>
+    <li key={props.currentSeries.id}>
       <div>
-        <img src={currentSeries.img_url} alt="qot" height="40%" width="40%" />
+        <img
+          src={props.currentSeries.img_url}
+          alt="qot"
+          height="40%"
+          width="40%"
+        />
       </div>
-      <h2>{currentSeries.name}</h2>
-      <h3>Rating: {currentSeries.rating}</h3>
-      <h4>Genre: {currentSeries.genre}</h4>
-      <p>Summary: {currentSeries.description}</p>
-      <p>Cast: {currentSeries.cast}</p>
-      <a href={currentSeries.watch_url} target="_blank">
+      <h2>{props.currentSeries.name}</h2>
+      <h3>Rating: {props.currentSeries.rating}</h3>
+      <h4>Genre: {props.currentSeries.genre}</h4>
+      <p>Summary: {props.currentSeries.description}</p>
+      <p>Cast: {props.currentSeries.cast}</p>
+      <a href={props.currentSeries.watch_url} target="_blank">
         <button>Watch Now</button>
       </a>
     </li>
