9cc7239c3a9878c6680ee0396dc28d1b0263dc0a
Meraj-Uddin-Malik <merajuddinmalik136@gmail.com>
2026-02-02 01:57:03 +0500

    style: wrap SeriesCard content in a card-content div for improved layout

--- src/components/SeriesCard.jsx
+++ src/components/SeriesCard.jsx
@@ -3,18 +3,21 @@ export const SeriesCard = (props) => {
   const { id, name, img_url, rating, genre, description, cast, watch_url } =
     props.data;
   return (
-    <li>
+    <li className="card">
       <div>
         <img src={img_url} alt="qot" height="40%" width="40%" />
       </div>
-      <h2>{name}</h2>
-      <h3>Rating: {rating}</h3>
-      <h4>Genre: {genre}</h4>
-      <p>Summary: {description}</p>
-      <p>Cast: {cast}</p>
-      <a href={watch_url} target="_blank">
-        <button>Watch Now</button>
-      </a>
+
+      <div className="card-content">
+        <h2>{name}</h2>
+        <h3>Rating: {rating}</h3>
+        <h4>Genre: {genre}</h4>
+        <p>Summary: {description}</p>
+        <p>Cast: {cast}</p>
+        <a href={watch_url} target="_blank">
+          <button>Watch Now</button>
+        </a>
+      </div>
     </li>
   );
 };
