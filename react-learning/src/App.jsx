5654521bf644abd2458e17e508e90013b944a026
Meraj-Uddin-Malik <merajuddin618@gmail.com>
2026-05-18 17:33:22 +0500

    feat: add developer details to App component

--- react-learning/src/App.jsx
+++ react-learning/src/App.jsx
@@ -4,7 +4,10 @@ export const App = () => {
     <div>
       <div>
         <img src="images.png" alt="Vinc" />
-        </div>
+      </div>
+      <h4>Name: Vincenzo</h4>
+      <p>rating: 4.5/5</p>
+      <p>summary: Vincenzo is a talented developer with a passion for creating innovative solutions.</p>
     </div>
   );
 }
