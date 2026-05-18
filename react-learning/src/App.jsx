fd89bd2d7fffce0668333f56648309992aac9420
Meraj-Uddin-Malik <merajuddin618@gmail.com>
2026-05-18 17:43:29 +0500

    refactor: streamline App component by removing duplicate code and enhancing structure

--- react-learning/src/App.jsx
+++ react-learning/src/App.jsx
@@ -3,56 +3,32 @@ export const App = () => {
   return (
     <>
       <div>
-      <div>
-        <img src="images.png" alt="Vinc" />
-      </div>
-      <h2>Name: Vincenzo</h2>
-      <h3>rating: 4.5/5</h3>
-      <p>summary: Vincenzo is a talented developer with a passion for creating innovative solutions.</p>
-    </div>
+        <div>
+          <img src="images.png" alt="Vinc" />
+        </div>
+        <h2>Name: Vincenzo</h2>
+        <h3>rating: 4.5/5</h3>
+        <p>summary: Vincenzo is a talented developer with a passion for creating innovative solutions.</p>
+      </div>    
+      
+      <NetflixSeries />
 
-
-      <div>
-      <div>
-        <img src="images.png" alt="Vinc" />
-      </div>
-      <h2>Name: Vincenzo</h2>
-      <h3>rating: 4.5/5</h3>
-      <p>summary: Vincenzo is a talented developer with a passion for creating innovative solutions.</p>
-    </div>
-
-      <div>
-      <div>
-        <img src="images.png" alt="Vinc" />
-      </div>
-      <h2>Name: Vincenzo</h2>
-      <h3>rating: 4.5/5</h3>
-      <p>summary: Vincenzo is a talented developer with a passion for creating innovative solutions.</p>
-    </div>
-
-
-      <div>
-      <div>
-        <img src="images.png" alt="Vinc" />
-      </div>
-      <h2>Name: Vincenzo</h2>
-      <h3>rating: 4.5/5</h3>
-      <p>summary: Vincenzo is a talented developer with a passion for creating innovative solutions.</p>
-    </div>
-
-      <div>
-      <div>
-        <img src="images.png" alt="Vinc" />
-      </div>
-      <h2>Name: Vincenzo</h2>
-      <h3>rating: 4.5/5</h3>
-      <p>summary: Vincenzo is a talented developer with a passion for creating innovative solutions.</p>
-    </div>
+      
     </>
-  
+  );
+}
 
 
-
+const NetflixSeries = () => {
+  return (
+    <div>
+      <div>
+        <img src="images.png" alt="Vinc" />
+      </div>
+      <h2>Name: Vincenzo</h2>
+      <h3>rating: 4.5/5</h3>
+      <p>summary: Vincenzo is a talented developer with a passion for creating innovative solutions.</p>
+    </div>
   );
 }
 
